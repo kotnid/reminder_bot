876f5cf16cb3969ce2a4dbf7a0d84669773ec9dd
kotnid <tkt0506tkt@gmail.com>
2022-03-19 17:45:13 +0800

    reminder_bot : Add pymongo setup to main.py
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from logging import info , basicConfig , INFO
+from pymongo import MongoClient
+from os import environ
 
 # logging config 
 basicConfig(level= INFO,
             format= '%(asctime)s %(levelname)s %(message)s',
             datefmt= '%Y-%m-%d %H:%M')
+
+# mongodb client setup
+client = MongoClient('mongodb+srv://tkt_bot_version1:{}@cluster0.tu30q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'.format(environ['pw']))
+db = client['reminder_bot']
+job_db = db['jobs']
